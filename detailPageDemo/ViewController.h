829f58d15cfd0e78347f94068cfe2c405adb470b
LongShengWen <1142056181@qq.com>
2018-06-28 11:12:10 +0800

    Initial Commit

---
+++ detailPageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  detailPageDemo
+//
+//  Created by 粟黄 on 2018/6/28.
+//  Copyright © 2018年 lsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ detailPageDemoTests/detailPageDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  detailPageDemoTests.m
+//  detailPageDemoTests
+//
+//  Created by 粟黄 on 2018/6/28.
+//  Copyright © 2018年 lsw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface detailPageDemoTests : XCTestCase
+
+@end
+
+@implementation detailPageDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
